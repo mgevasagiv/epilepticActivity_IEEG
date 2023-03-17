cab0e7b7cb318a97dd5d276b498e319886fe72aa
Maya Geva-Sagiv <mgevasagiv@ucdavis.edu>
2023-03-17 10:49:46 -0700

    Update spikeDetector_example.m

--- spikeDetector_example.m
+++ spikeDetector_example.m
@@ -1,9 +1,11 @@
-IIS_det = SpikeWaveDetector;
-load('E:\Data_p\p487\EXP3\p487_EXP3_dataset.mat');
-load('E:\Data_p\MACRO_MONTAGE\p487\EXP3\MacroMontage.mat')
-dataFolder = header.processed_MACRO;
+IIS_det = SpikeWaveDetector; % see SpikeWaveDetector.docx for details on functions and default parameters
+
+dataFolder = header.processed_MACRO; % extracted iEEG files
+% IIS_det.samplingRate default is 1kHz - update if different
+
 channels = [1, 9, 16, 24, 31, 38, 45, 53, 60, 67]; % deepest contact for each electrode
 
+% Extract interictal activities from all channels
 clear peakTimes mlink passedConditions
 for ii = 1:length(channels)
     filename = fullfile(dataFolder, sprintf('CSC%d.mat',channels(ii)));
@@ -11,4 +13,5 @@ for ii = 1:length(channels)
     [peakTimes{ii}, passedConditions{ii}]= detectTimes(IIS_det, mlink{ii}.data,true);
 end
 
+% Plot IED detected on given channels
 plotSpikeWaves(IIS_det, mlink{ii}.data, peakTimes{ii},1, passedConditions{ii});
